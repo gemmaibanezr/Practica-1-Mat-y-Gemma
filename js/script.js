4e7a1d16570f36d9d8290ccc54a670538609c75b
guisandodesign <marta.estebanez@alumnos.udit.es>
2025-11-14 21:45:48 +0100

    cursor

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ const menu = document.getElementById('menu-overlay');
 hamburger.addEventListener('click', () => {
 menu.classList.toggle('active');
 });
+
+
+// Cursor degradado
+const cursor = document.getElementById("cursor");
+
+document.addEventListener("mousemove", e => {
+  cursor.style.left = `${e.clientX}px`;
+  cursor.style.top = `${e.clientY}px`;
+});
